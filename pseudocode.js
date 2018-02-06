aedb2ed1f785759c588b51763bc4c9cb2a11f342
Aaron Shafovaloff <aaronshaf@gmail.com>
2018-02-05 17:36:16 -0700

    Update pseudocode.js

--- pseudocode.js
+++ pseudocode.js
@@ -1,5 +1,14 @@
-// ...
+// start raf loop on first call
+// in every loop save state/cache of gamepads
+// deliever previous and current for each
 
-const args => inputSource => outputSource => {
-
+// copypasta fro now
+const multiplyBy = args => inputSource => {
+  return function outputSource(start, outputSink) {
+    if (start !== 0) return;
+    inputSource(0, (type, data) => {
+      if (type === 1) outputSink(1, data);
+      else outputSink(t, d);
+    });
+  };
 }
