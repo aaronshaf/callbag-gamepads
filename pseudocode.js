09d51d464a100c13e9668a121690010c1ac56a13
Aaron Shafovaloff <aaronshaf@gmail.com>
2018-02-05 17:41:32 -0700

    Update pseudocode.js

--- pseudocode.js
+++ pseudocode.js
@@ -3,15 +3,13 @@
 // deliever previous and current for each
 
 // copypasta for now
-function source(type, data) {
-  if (type === 0) {
-    const sink = data;
-    let handle = setInterval(() => {
-      sink(1, null);
-    }, 1000);
-    const talkback = (t, d) => {
-      if (t === 2) clearInterval(handle);
-    };
-    sink(0, talkback);
-  }
+function source(start, sink) {
+  if (start !== 0) return;
+  let handle = setInterval(() => {
+    sink(1, null);
+  }, 1000);
+  const talkback = (t, d) => {
+    if (t === 2) clearInterval(handle);
+  };
+  sink(0, talkback);
 }
